39dabb751bf8170e5a70cf0db5894ad3923e3cb9
Tanmay Garg <tanmay17061@gmail.com>
2023-09-09 16:19:51 +0530

    create stubs for pipelines: train and add_new_samples

---
+++ src/pipelines/__init__.py
@@ -0,0 +1,2 @@
+from .train import train_pipeline
+from .add_new_samples import add_new_samples_pipeline
---
+++ src/pipelines/train.py
@@ -0,0 +1,4 @@
+import logging
+
+def train_pipeline(args):
+    logging.info("inside train_pipeline")
