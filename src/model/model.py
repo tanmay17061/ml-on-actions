d224ca2d9d507b0330f12205f07bd11e267410a0
Tanmay Garg <tanmay17061@gmail.com>
2023-09-10 14:29:20 +0530

    model should not be fit twice, inferenced without fitting first

--- src/model/model.py
+++ src/model/model.py
@@ -1,15 +1,26 @@
 # This is an example model implementation. The actual model architecture, hyperparameters can
 # vary per use-case basis.
 
+import logging
 from sklearn.svm import SVC
 import numpy as np
 
 class SVCModel:
     def __init__(self,hparams):
+
         self.clf = SVC(hparams.C, hparams.kernel)
+        self.has_been_fit = False
 
     def fit(self, X: np.ndarray, y: np.ndarray):
+
+        if self.has_been_fit:
+            logging.warn(f"calling fit on an already fitted {self.__class__.__name__} object.")
+
         self.clf.fit(X,y)
     
     def predict(self,X: np.ndarray):
+
+        if not self.has_been_fit:
+            raise ValueError(f"calling predict on {self.__class__.__name__} object without calling fit first.")
+
         return self.clf.predict(X)
